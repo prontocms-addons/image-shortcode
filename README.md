759f59648b4bf679dd7d60c2efc00b3f26184b5e
Thomas Rasshofer <me@thomasrasshofer.com>
2013-09-15 01:03:52 +0200

    Moving to GitHub (http://prontocms.com/blog/github)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Image shortcode add-on for PRONTO.
+ 
+[http://prontocms.com/downloads/image-shortcode](http://prontocms.com/downloads/image-shortcode)
---
+++ shortcode.image.php
@@ -0,0 +1,26 @@
+<?php
+
+use Pronto\ShortcodeContainer;
+use Pronto\ConfigContainer;
+use Pronto\GlobalContainer;
+use Pronto\HelperContainer;
+
+ShortcodeContainer::add('image', function($attributes) {
+	$page = GlobalContainer::get('page');
+	$image = array_shift($attributes);
+	if(HelperContainer::relative($image)) {
+		$image = $page->folder().$image;
+	}
+	$defaults = array(
+		'alt' => ''
+	);
+	$options = array_merge($defaults, $attributes);
+	$append = array();
+	foreach($options as $key => $val) {
+		$append[] = ' '.$key.'="'.html($val).'"';
+	}
+	$xhtml = ConfigContainer::get('xtml') ? ' /' : '';
+	return '<img src="'.$image.'"'.implode($append).$xhtml.'>';
+});
+
+?>
